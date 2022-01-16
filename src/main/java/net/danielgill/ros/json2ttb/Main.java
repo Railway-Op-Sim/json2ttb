af1b810a239421b5b1b211d187a47cb70e03375e
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-01-16 21:35:03 +0000

    Added log4j2

--- src/main/java/net/danielgill/ros/json2ttb/Main.java
+++ src/main/java/net/danielgill/ros/json2ttb/Main.java
@@ -1,6 +1,9 @@
 package net.danielgill.ros.json2ttb;
 
 import net.danielgill.ros.json2ttb.json.JSONTimetable;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.json.simple.parser.ParseException;
 
 import java.io.File;
@@ -8,19 +11,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
+    private static Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException, ParseException {
         if(args.length < 1) {
-            System.err.println("You have not provided a file, please try again.");
+            logger.error("You have not provided a file, please try again.");
             System.exit(0);
         }
         
         File file = new File(args[0]);
         JSONTimetable json = new JSONTimetable(file);
         String ttb = json.createTimetable();
-        
-        System.out.println(file.getAbsolutePath());
-        
         File outputFile = new File(file.getAbsolutePath().replace(".json", ".ttb"));
+        logger.info("Output file will be: " + outputFile.getAbsolutePath());
+
         FileWriter fw = new FileWriter(outputFile);
         fw.write(ttb);
         fw.close();
